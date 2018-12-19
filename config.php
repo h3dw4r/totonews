32f23688f8baf1b86893acb92b2373d26a0fb781
h3dw4r <38338029+h3dw4r@users.noreply.github.com>
2018-12-19 14:48:52 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBinuVuuCBbLPLlJexo4Zrfpyzy4LMcpvAeIqbkaC_3X6toZavmIVpv857y62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=1e937ae585b73b02",
+  ];
+
+?>
